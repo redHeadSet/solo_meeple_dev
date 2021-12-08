fe97e8c258e7f406a68466760e92a5223c72cff5
redHeadSet <stikfascube@gmail.com>
2021-12-08 16:26:17 +0900

    Init

---
+++ src/main/java/mozziyulmu/meeple/MeepleApplication.java
@@ -0,0 +1,13 @@
+package mozziyulmu.meeple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeepleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeepleApplication.class, args);
+	}
+
+}
